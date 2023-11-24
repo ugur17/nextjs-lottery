9d62a697a05579e88f45dfc5e79ecf897ae03f4f
Ceyhun Uğur <ceyhunugur@ceyhuns-air.mynetworksettings.com>
2023-11-24 03:10:09 -0500

    dependencies reinstalled

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 // import ManualHeader from '@/components/ManualHeader'
 import Header from '@/components/Header'
 import LotteryEntrance from '@/components/LotteryEntrance'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
